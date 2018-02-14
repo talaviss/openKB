1b8211b8d59e6b18fc0abf97dce3b4126d8693b3
Tal Avissar <Tal.Avissar@amdocs.com>
2018-02-14 15:18:52 +0200

    more fixes to couch

--- app.js
+++ app.js
@@ -338,7 +338,9 @@ if(config.settings.database.type === 'embedded'){
         console.error('With couchbase must specify bucket name in config json');
         process.exit();
     }
-    var bucket = cluster.openBucket(config.settings.database.bucket_name,  function(err) {
+    var bucket = cluster.openBucket(config.settings.database.bucket_name);
+    var N1qlQuery = couchbase.N1qlQuery;
+   /* var bucket = cluster.openBucket(config.settings.database.bucket_name,  function(err) {
         if (err) {
             console.error('Got error: %j', err);
         }
@@ -349,10 +351,25 @@ if(config.settings.database.type === 'embedded'){
                 console.error("Couldn't store document: %j", err);
             }
         });
+    });*/
+
+
+    bucket.manager().createPrimaryIndex(function() {
+        bucket.upsert('user:king_arthur', {
+                'email': 'kingarthur@couchbase.com', 'interests': ['Holy Grail', 'African Swallows']
+            },
+            function (err, result) {
+                bucket.get('user:king_arthur', function (err, result) {
+                    console.log('Got result: %j', result.value);
+                    bucket.query(
+                        N1qlQuery.fromString('SELECT * FROM ' + config.settings.database.bucket_name +  '  WHERE $1 in interests LIMIT 1'),
+                        ['African Swallows'],
+                        function (err, rows) {
+                            console.log("Got rows: %j", rows);
+                        });
+                });
+            });
     });
-    var N1qlQuery = couchbase.N1qlQuery;
-
-
 
 
     /*bucket.manager().createPrimaryIndex(function() {
